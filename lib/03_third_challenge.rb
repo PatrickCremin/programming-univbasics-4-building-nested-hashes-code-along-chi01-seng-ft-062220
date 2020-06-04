7bd1075f68218f46ef9ac2bc042e86e2ac714bd8
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-04 20:26:46 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -6,7 +6,7 @@ def third_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {name: "Benvolio" , age: "17" , status: "worried"},
-        
+        {name: "Mercutio" , age: "18" , status: "hot-headed"}
         ]
    },
    :capulet => {
