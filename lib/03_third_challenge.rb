71ca92a4b4906f2e15a404e221ee29e0d401fbeb
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-04 20:21:03 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -24,8 +24,10 @@ def third_challenge
         :name => "Lady Capulet",
       :age => "51"
         },
-      :heroine => {
-        #your key/value pairs here
+      :heroine => {:name => "Juliet",
+      :age => "15",
+      :status => "alive"
+      
         },
       :heroine_friends => []
    }
