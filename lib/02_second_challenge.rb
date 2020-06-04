6091400708011c415f6a7a488e167fe082682877
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-04 20:13:25 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -19,7 +19,9 @@ def second_challenge
       }]
    },
    :capulet => {
-      :patriarch => {},
+      :patriarch => {{
+      :name => "Lord Capulet",
+      :age => "50"},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
