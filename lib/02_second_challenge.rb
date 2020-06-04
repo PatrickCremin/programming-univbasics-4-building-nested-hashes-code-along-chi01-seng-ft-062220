fbf97ecf312f54f470011ae7f89c86fbade08f88
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-04 20:14:07 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -27,7 +27,16 @@ def second_challenge
       :heroine => {:name => "Juliet",
       :age => "15",
       :status => "alive"},
-      :heroine_friends => []
+      :heroine_friends => [{
+        :name => "Steven",
+        :age => "30",
+        :attitude => "confused"
+      },
+      {
+        :name => "Nurse",
+        :age => "44",
+        :attitude => "worried"
+      }]
    }
 }
   
