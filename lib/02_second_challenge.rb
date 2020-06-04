e62e37f0608dc027d4515e5ef7fe83996c296c8b
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-04 20:07:17 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,3 +1,15 @@
 def second_challenge
-  
+  :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
 end
