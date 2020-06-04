6f7950ac1765655117a75e4bf2c77f37aeaeb41c
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-04 20:11:58 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -3,8 +3,14 @@ def second_challenge
    :montague => {
       :patriarch => {:patriarch => {
       :name => "Lord Montague",
-      :age => "53"},
-      :matriarch => {},
+      :age => "53"
+        
+      },
+      :matriarch => {:matriarch => {
+      :name => "Lady Montague",
+      :age => "54"
+        
+      },
       :hero => {},
       :hero_friends => []
    },
